2f092fb7ffa0239f3145fd3c5b5dff98c3c892cf
CriMilanese <cmilanese@pop-os.localdomain>
2020-05-31 19:42:59 +0200

    small updates

--- server/src/chat.c
+++ server/src/chat.c
@@ -9,7 +9,7 @@
 #include <pthread.h>
 #include "queue.h"
 
-#define MAX 1024
+#define MAX_BUFFER 1024 * 1024 // 2 MB
 #define PORT 8080
 #define SA struct sockaddr
 #define BACKLOG 10
@@ -25,40 +25,39 @@ pthread_cond_t cond_var = PTHREAD_COND_INITIALIZER;
 /*
 *  This function is designed to send data to another device and read
 *  the response back.
-*  PARAM: file descriptor of opened sockets
+*  PARAM: file descriptor of opened socket
 */
 void communicate(int *sockfd, FILE *logfd){
-  // int curr_fd = *sockfd;
-  char *buff = malloc(MAX * sizeof(char));
+  char *buff = malloc(MAX_BUFFER * sizeof(char));
+  int bytes_transferred  = 0;
 
   // fill buffer with zeros
-  memset(buff, 0, MAX*sizeof(char));
-
+  memset(buff, 0, MAX_BUFFER*sizeof(char));
   while(1){
       // read the message from client and copy it in buffer
-      while(recv(*sockfd, buff, MAX*sizeof(char), 0)<0);
+      bytes_transferred = recv(*sockfd, buff, MAX_BUFFER*sizeof(char), 0);
 
+      fprintf(logfd, "bytes received: %d\n", bytes_transferred);
       // the last iteration we break early by sending an exit message
-      // to the client, which will terminate its loop too
+      // to the server, which terminates the communication
       if(buff[0]==1)
         break;
       // and send that buffer back to client
-      send(*sockfd, buff, MAX*sizeof(char), 0);
+      bytes_transferred = send(*sockfd, buff, bytes_transferred*sizeof(char), 0);
+      fprintf(logfd, "bytes sent: %d\n", bytes_transferred);
   }
   free(buff);
 }
 
+// main thread function, each thread in the pool will loop through trying to get the lock
+// and one will wait on a conditional variable to be signaled, meaning there is
+// a job to dequeue
 void *thread_func(void *input){
-  // struct Queue *q_tmp = (struct Queue *)input;
-  // free(input);
   int active_fd = 0;
   while(1){
     pthread_mutex_lock(&mutex);
-    // fprintf((FILE *)input, "from other thread\n");
-    // print_queue((FILE *)input, q);
     pthread_cond_wait(&cond_var, &mutex);
     active_fd = dequeue(q);
-    // fprintf((FILE *)input, "%d\n", active_fd);
     pthread_mutex_unlock(&mutex);
     if(active_fd != 0){
       communicate(&active_fd, (FILE *)input);
@@ -76,10 +75,8 @@ int main(){
   q = createQueue(MAX_JOBS);
   //local copy of list, cause select is disruptive
   fd_set master_read;
-  // fd_set master_write;
   // temporary file descriptor list for select()
   fd_set read_fds;
-  // fd_set write_fds;
 
   // open a new fd instead of using stdout and stderr
   logfd = fopen("bench/server/log/server_log.txt", "w+");
@@ -98,7 +95,7 @@ int main(){
 
   // /*"address already in use" error message */
   if(setsockopt(master_socket_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(int)) == -1){
-    fprintf(logfd, "ERROR: Server-setsockopt() failed\n");
+    fprintf(logfd, "ERROR: Server setsockopt() failed\n");
     return EXIT_FAILURE;
   }
 
@@ -126,9 +123,7 @@ int main(){
 
   // Accept the data packet from client and verification
   FD_ZERO(&master_read);
-  // FD_ZERO(&master_write);
   FD_ZERO(&read_fds);
-  // FD_ZERO(&write_fds);
 
   /* add the listening file descriptor to the master set */
   FD_SET(master_socket_fd, &master_read);
@@ -145,12 +140,11 @@ int main(){
   while(1){
     /* copy fds over, because select is disrupting */
     read_fds = master_read;
-    // write_fds = master_write;
 
+    int rt = select(maxfd+1, &read_fds, NULL, NULL, &expiration);
     /* reinitialize timeout value*/
     expiration.tv_sec = TIMEOUT;
 
-    int rt = select(maxfd+1, &read_fds, NULL, NULL, &expiration);
     if((rt) == -1){
       fprintf(logfd,"ERROR: server select() failed..\n");
       return EXIT_FAILURE;
