36a64a88677ff3f610dc1b546ff03a0bd1a31daa
CriMilanese <cmilanese@pop-os.localdomain>
2020-06-15 03:06:58 +0200

    non-blocking socket to receive the most

--- client/src/client.c
+++ client/src/client.c
@@ -6,10 +6,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <errno.h>
 #include "client.h"
 
-#define MAX_BUFFER 1024 * 1024 // 1 MB
-#define TRANSFER_SIZE 4096
+#define MAX_BUFFER 4 * 1024
 #define RECV_CHUNK_SIZE 512
 #define PORT 8080
 #define SA struct sockaddr
@@ -20,7 +20,9 @@
 #define RTT_OPTION 0
 #define TIMEOUT 5
 
-int recv_all(int sockfd, FILE *logfd, ssize_t bytes_sent){
+static float *results;
+
+int recv_all(int *sockfd, FILE *logfd){
   // receive buffer init
 	char *chunk = malloc(RECV_CHUNK_SIZE*sizeof(char));
 	fd_set set;
@@ -32,7 +34,7 @@ int recv_all(int sockfd, FILE *logfd, ssize_t bytes_sent){
 
 	while(1){
 		FD_ZERO(&set);
-		FD_SET(sockfd, &set);
+		FD_SET(*sockfd, &set);
 
 		// clear the receiving buffer
 		memset(chunk, 0, RECV_CHUNK_SIZE*sizeof(char));
@@ -48,77 +50,65 @@ int recv_all(int sockfd, FILE *logfd, ssize_t bytes_sent){
 			fprintf(logfd, "receiving timeout..\nexit..\n");
 			break;
 		} else {
-			if(FD_ISSET(sockfd, &set)){
-				bytes_recv = recv(sockfd, chunk, RECV_CHUNK_SIZE*sizeof(char), 0);
-				fprintf(logfd, "%d bytes were received\n", bytes_recv);
-				total_recv += bytes_recv;
+			if(FD_ISSET(*sockfd, &set)){
+				if((bytes_recv = recv(*sockfd, chunk, RECV_CHUNK_SIZE*sizeof(char), 0))<0){
+					if(errno == EAGAIN || errno == EWOULDBLOCK){
+						fprintf(logfd, "recv would block\n");
+						// break;
+					} else {
+						fprintf(logfd, "receiving chunk error\n");
+					}
+				} else {
+					// fprintf(logfd, "%d bytes were received\n", bytes_recv);
+					total_recv += bytes_recv;
+				}
 			} else {
 				fprintf(logfd, "sockfd is not set (FD_ISSET)\n");
 			}
 		}
-		FD_CLR(sockfd, &set);
+		FD_CLR(*sockfd, &set);
 	}
 
 	free(chunk);
 	return total_recv;
 }
 
-float *communicate(int sockfd, FILE *logfd, float* results)
-{
-	char *buff = malloc(MAX_BUFFER * sizeof(char));
+float *communicate(int *sockfd, int lifespan, FILE *logfd){
+	const char *msg = ;
 	struct timeval curr_tm, rtt_tm, bw_tm;
   long sum_rtt, sum_bw = 0;
 	int n = 0;
 	int bytes_transferred = 0;
 
-	memset(buff, 0, MAX_BUFFER*sizeof(char));
-	while(n <= TESTS){
-
-		if (n == TESTS) {
-			memset(buff, 1, MAX_BUFFER*sizeof(char));
-      send(sockfd, buff, TRANSFER_SIZE*sizeof(char), 0);
-			break;
-		}
-		gettimeofday(&rtt_tm, NULL);
-		bytes_transferred = send(sockfd, buff, TRANSFER_SIZE*sizeof(char), 0);
-		if(bytes_transferred < 0){
-			fprintf(logfd, "send error\n");;
-		} else {
-			fprintf(logfd, "bytes sent: %d\n", bytes_transferred);
-		}
-		gettimeofday(&bw_tm, NULL);
-		bytes_transferred = recv_all(sockfd, logfd, TRANSFER_SIZE*sizeof(char));
-		gettimeofday(&curr_tm, NULL);
-
-		fprintf(logfd, "total bytes received: %d\n", bytes_transferred);
-
-		sum_rtt += ((curr_tm.tv_sec*1e6 + curr_tm.tv_usec) - (rtt_tm.tv_sec*1e6 + rtt_tm.tv_usec));
-		sum_bw += ((curr_tm.tv_sec*1e6 + curr_tm.tv_usec) - (bw_tm.tv_sec*1e6 + bw_tm.tv_usec));
-		n++;
+	gettimeofday(&rtt_tm, NULL);
+	bytes_transferred = send(*sockfd, msg, strlen(msg), 0);
+	if(bytes_transferred < 0){
+		fprintf(logfd, "send error\n");;
+	} else {
+		fprintf(logfd, "bytes sent: %d\n", bytes_transferred);
 	}
+	gettimeofday(&bw_tm, NULL);
+	bytes_transferred = recv_all(sockfd, logfd);
+	gettimeofday(&curr_tm, NULL);
+
+	fprintf(logfd, "total bytes received: %d\n", bytes_transferred);
+
+	sum_rtt += ((curr_tm.tv_sec*1e6 + curr_tm.tv_usec) - (rtt_tm.tv_sec*1e6 + rtt_tm.tv_usec));
+	sum_bw += ((curr_tm.tv_sec*1e6 + curr_tm.tv_usec) - (bw_tm.tv_sec*1e6 + bw_tm.tv_usec));
 	results[RTT_OPTION] = (float)sum_rtt/TESTS;
 	results[BW_OPTION] = ((bytes_transferred/TESTS)*1e6/((float)sum_bw/TESTS));
-	free(buff);
 	return results;
 }
 
-
-int main(int argc, char const *argv[])
-{
+float start(char* target_server, int lifetime){
 	int sockfd, connfd;
-	float *results = malloc(RESULTS_NUM * sizeof(float));
+	results = malloc(RESULTS_NUM*sizeof(float));
 	struct sockaddr_in servaddr;
 	FILE *logfd;
 
-	logfd = fopen("bench/client/log/client_log.txt", "a+");
+	logfd = fopen("../client/log/client_log.txt", "a+");
 	if(logfd == NULL){
 		printf("CLIENT LOG FILE FAILED\n");
-		return EXIT_FAILURE;
-	}
-
-	if(argc < 2){
-		fprintf(logfd, "ERROR: target server ip missing\n");
-		fclose(logfd);
 		return EXIT_FAILURE;
 	}
 
@@ -133,9 +123,10 @@ int main(int argc, char const *argv[])
 	}
 	bzero(&servaddr, sizeof(servaddr));
 
+	fprintf(logfd, "[DEBUG] trying to connect to: %s\n", target_server);
 	// assign IP, PORT
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+	servaddr.sin_addr.s_addr = inet_addr(target_server);
 	servaddr.sin_port = htons(PORT);
 
 	// connect the client socket to server socket
@@ -144,6 +135,7 @@ int main(int argc, char const *argv[])
 		if(trials == MAX_TRIALS){
 			fprintf(logfd, "ERROR: server was unreachable\n");
 			fclose(logfd);
+			close(sockfd);
 			return EXIT_FAILURE;
 		}
 		sleep(1);
@@ -155,12 +147,21 @@ int main(int argc, char const *argv[])
   fcntl(sockfd, F_SETFL, O_NONBLOCK);
 
 	// function for chat
-	results = communicate(sockfd, logfd, results);
+	results = communicate(&sockfd, lifetime, logfd);
 	fprintf(logfd, "avg round trip time with %s is %.2f us\n", inet_ntoa(servaddr.sin_addr), results[RTT_OPTION]);
 	fprintf(logfd, "avg bandwidth with %s is %.2f KB/s\n", inet_ntoa(servaddr.sin_addr), results[BW_OPTION]);
+	fprintf(logfd, "supposed lifetime: %d\n", lifetime);
 	// close the file pointers
-	free(results);
 	fclose(logfd);
 	close(sockfd);
-  return EXIT_SUCCESS;
+  return results[BW_OPTION];
+}
+
+void free_results(){
+	free(results);
+}
+
+// to please compiler
+int main(){
+	return 0;
 }
