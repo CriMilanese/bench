323d0949abe0cb1fd41be8393d07579d43a2037b
CriMilanese <cmilanese@pop-os.localdomain>
2020-06-15 03:06:00 +0200

    proper documentation

--- client/inc/client.h
+++ client/inc/client.h
@@ -1,8 +1,33 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-float *communicate(int sockfd, FILE *logfd, float* results);
-int recv_all(int sockfd, FILE *logfd, ssize_t bytes_sent);
 
+/*
+* the function gauges the time and bytes send to this very client
+* and performs the maths to return the results for this test
+* PARAM:
+*  char* is the target server local IP address
+*  int is the user-defined timeout for the test
+*/
+float start(char*, int);
+
+/*
+* the function gauges the time and bytes send to this very client
+* and performs the maths to return the results for this test
+* PARAM:
+*  int* is the file descriptor obtained from connecting with the server
+*  int is the user-defined timeout for the test
+*  FILE* log file descriptor
+*/
+float *communicate(int*, int, FILE*);
+
+/*
+* uses non blocking sockets in a select loop to keep reading until
+* max bandwidth is reached or there is no more data.
+* PARAM:
+*  int* is the file descriptor obtained from connecting with the server
+*  FILE* log file descriptor
+*/
+int recv_all(int*, FILE*);
 
 #endif //CLIENT_H
