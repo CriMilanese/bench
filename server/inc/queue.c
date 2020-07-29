c733f505232271db9ae3840f16dbec48188731b4
CriMilanese <cmilanese@pop-os.localdomain>
2020-07-29 02:09:41 +0200

    item is a node struct

--- server/inc/queue.c
+++ server/inc/queue.c
@@ -4,18 +4,16 @@
 #include <string.h>
 #include "queue.h"
 
-// function to create a queue of given capacity.
 struct Queue* createQueue(int capacity){
   struct Queue* queue = (struct Queue*) calloc(1, sizeof(struct Queue));
   queue->capacity = capacity;
   queue->size = 0;
-  queue->head = (int*) calloc(queue->capacity, sizeof(struct Node));
+  queue->head = (struct Node*) calloc(queue->capacity, sizeof(struct Node));
   queue->front = queue->rear = queue->head;
   queue->tail = queue->head + queue->capacity - 1;
   return queue;
 }
 
-// Queue is full when size becomes equal to the capacity
 int is_full(struct Queue* queue){
   return (queue->size == queue->capacity);
 }
@@ -24,13 +22,12 @@ int is_empty(struct Queue* queue){
   return (queue->size == 0);
 }
 
-// Function to add an item to the queue.
-int *enqueue(struct Queue* queue, int item, int lf){
+struct Node *enqueue(struct Queue* queue, int item, int lf){
   if (is_full(queue))
       return NULL;
-  int *tmp = &item;
-  
-  *(queue->rear) = item;
+
+  struct Node n = {item, lf};
+  *queue->rear = n;
 
   // make it circular
   if(queue->rear == queue->tail){
@@ -39,15 +36,14 @@ int *enqueue(struct Queue* queue, int item, int lf){
     queue->rear += 1;
   }
 
-  queue->size = queue->size + 1;
-  return tmp;
+  queue->size += 1;
+  return queue->rear;
 }
 
-// Function to remove an item from queue.
-int dequeue(struct Queue* queue){
+struct Node *dequeue(struct Queue* queue){
   if (is_empty(queue))
-      return 0;
-  int *item = queue->front;
+      return NULL;
+  struct Node *item = queue->front;
 
   // make it circular
   if(queue->front == queue->tail){
@@ -56,31 +52,31 @@ int dequeue(struct Queue* queue){
     queue->front += 1;
   }
 
-  queue->size = queue->size - 1;
-  return *item;
+  queue->size -= 1;
+  return item;
 }
 
-// Function to get front of queue
-int *front(struct Queue* queue){
+struct Node *forefront(struct Queue* queue){
   if (is_empty(queue))
-      return 0;
+      return NULL;
   return queue->front;
 }
 
-// Function to get rear of queue
-int *back(struct Queue* queue){
+struct Node *back(struct Queue* queue){
   if (is_empty(queue))
-      return 0;
+      return NULL;
   return queue->rear;
 }
 
+// TODO: take out in production
 void print_queue(FILE* fd, struct Queue *queue){
   int sz = queue->size;
   fprintf(fd, "Printing the content of queue..\n");
   fprintf(fd, "size is: %d\n", sz);
-  int *tmp = queue->front;
+  struct Node *tmp = queue->front;
+  fprintf(fd, "%d\n", (queue->front)->lifetime_left);
   for(int i=0; i<sz; i++){
-    fprintf(fd, "element %d is %d\n", i, *(queue->front));
+    fprintf(fd, "element %d is (sfd: %d, ll: %d)\n", i, (queue->front)->socket_fd, (queue->front)->lifetime_left);
     if(queue->front == queue->tail){
       queue->front = queue->head;
     } else {
@@ -91,6 +87,9 @@ void print_queue(FILE* fd, struct Queue *queue){
 }
 
 void free_queue(struct Queue *queue){
+  // for(int i=queue->size; i>0;i--){
+  //   free(queue->head[i])
+  // }
   free(queue->head);
   free(queue);
 }
--- server/inc/queue.h
+++ server/inc/queue.h
@@ -14,29 +14,38 @@ struct Node {
 
 // A linked list structure to represent a queue
 struct Queue {
-  Node *head;
-  int *tail;
-  int *front;
-  int *rear;
+  struct Node *head;
+  struct Node *tail;
+  struct Node *front;
+  struct Node *rear;
   int size, capacity;
 };
+
+// typedef struct Queue q;
 
 // initiate a queue by defining its max size
 struct Queue* createQueue(int);
 
-//  the following operations take the queue object as thieir first parameters
+/*
+  add the socket file descriptor and its expected lifetime. It first checks whether
+  the queue is full, in which case it will return NULL, otherwise it will go ahead
+  and create a new Node item containg the given parameters. Enqueuing returns
+  the item just for
+*/
+struct Node *enqueue(struct Queue*, int, int);
+struct Node *dequeue(struct Queue*);
+
+// retuns the front pointer
+struct Node *forefront(struct Queue*);
+
+// returns the rear pointer
+struct Node *back(struct Queue*);
+
+// a user needs to use this function to clean up the queue allocated memory
+void free_queue(struct Queue*);
+
 int is_full(struct Queue*);
 int is_empty(struct Queue*);
-
-/*
-  it requires a extra two parameters, the socket file descriptor and its
-  expected lifetime
-*/
-int *enqueue(struct Queue*, int, int);
-int dequeue(struct Queue*);
-int *front(struct Queue*);
-int *back(struct Queue*);
-void free_queue(struct Queue*);
 void print_queue(FILE*, struct Queue*);
 
 #endif
