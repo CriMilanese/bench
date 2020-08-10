b8f0dfc82c16087052cf62557af03f0fea2f4185
CriMilanese <cmilanese@pop-os.localdomain>
2020-08-11 00:13:09 +0200

    lighten imports and pydocs

--- grpc/slave.py
+++ grpc/slave.py
@@ -1,24 +1,28 @@
-import grpc
+from grpc import server
 from concurrent import futures
-import time
-import sys
-sys.path.append('../utils');
+from time import sleep
+from sys import path, exit
+path.append('../utils');
 from signal import signal, SIGINT, SIGTERM
-
-#import classes for c library
 from ctypes import *
 from subprocess import call
-
-# import classes for grpc server
 import engage_pb2
 import engage_pb2_grpc
 
 # create a class to define the server functions, derived from
 # engage_pb2_grpc.engageServicer
 class EngageServer(engage_pb2_grpc.TestServicer):
-
+    """
+        extends the class generated from grpc compilation, to include
+        the custom functions that will run based on instructions given
+        by the master script, in order to execute the corresponding program
+    """
     def terminal(self, lifetime, target):
-        result = ''
+        """
+            discriminates on the lifetime value to run either a server or a
+            client on this machine
+        """
+        result = None
         if target:
             lib = CDLL("../client/liblclient.so")
             lib.start.argtypes = [c_char_p, c_int]
@@ -26,32 +30,33 @@ class EngageServer(engage_pb2_grpc.TestServicer):
             target = c_char_p(target.encode('utf-8'))
             result = lib.start(target, lifetime)
         else:
-            result = call("../server/server", shell=True)
+            result = call("cd ~/bench/server; ./server", shell=True)
         return result
 
     def Engage(self, request, context):
+        """
+            dispatches (?) the requested roles
+        """
         metadata = dict(context.invocation_metadata())
         res = self.terminal(request.lifetime, request.target)
         metrics = engage_pb2.Metrics(bandwidth=res)
         return metrics
 
 # create a gRPC server passing a filled thread pool to handle incoming requests
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
+grpc_server = server(futures.ThreadPoolExecutor(max_workers=4))
 
 # use the generated function `add_engageServicer_to_server`
 # to add the defined class to the server
 engage_pb2_grpc.add_TestServicer_to_server(
-        EngageServer(), server)
+        EngageServer(), grpc_server)
 
-# listen on port 50051
-with open("log", 'w') as log:
-    log.write('Slave ready to receive instructions. Listening on port 50051.')
-server.add_insecure_port('[::]:50051')
-server.start()
+# listen on localhost at port 50051
+grpc_server.add_insecure_port('[::]:50051')
+grpc_server.start()
 
 def closing(*args):
-    server.stop(0)
-    sys.exit(0)
+    grpc_server.stop(0)
+    exit(0)
 
 signal(SIGINT, closing)
 signal(SIGTERM, closing)
@@ -60,6 +65,6 @@ signal(SIGTERM, closing)
 # a sleep-loop is added to keep alive
 try:
     while True:
-        time.sleep(5)
+        sleep(1024)
 except KeyboardInterrupt:
-    server.stop(0)
+    grpc_server.stop(0)
