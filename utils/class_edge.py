c4e32d954d97445be943a14974eacabf8c3839ea
CriMilanese <cmilanese@pop-os.localdomain>
2020-08-25 05:05:01 +0200

    more pydoc less print

--- utils/class_edge.py
+++ utils/class_edge.py
@@ -2,24 +2,28 @@
     the class describing a single connection between two hosts
 """
 from tkinter import LAST, Label, StringVar
-from globals import WHITE, LIGHT_BLUE
+from globals import WHITE, LIGHT_BLUE, root
+from math import floor
+
 class Edge():
 
     def __init__(self, source, target, lf):
         self.source = source
         self.dest = target
         self.lifetime = lf
-        self.result = ''
+        self.string_result = ''
 
-    # pass two instances of node to draw a line in between
     def show(self, anchor, a, b):
+        """
+            pass two instances of node to draw a line in between
+            this is for the gui to be able to change the connection label on the fly
+            as each client returns the results
+        """
         self.body = anchor.create_line(a.x, a.y, b.x, b.y, arrow=LAST)
-        # this is for the gui to be able to change the connection label on the fly
-        # as each client returns the results
-        # self.result = StringVar()
+        self.result = StringVar()
         self.label = Label(anchor, textvariable=self.result, fg=WHITE, bg=LIGHT_BLUE)
-        self.middle = ((a.x+b.x)/2, (a.y+b.y)/2)
-        self.label.place(x=self.middle[0], y=self.middle[1])
+        middle = ((a.x+b.x)/2, (a.y+b.y)/2)
+        self.label.place(x=middle[0], y=middle[1])
 
     def outcome(self, caller):
         """
@@ -27,16 +31,14 @@ class Edge():
             the edge on the GUI to update with the reported bandwidth for that
             connection
         """
-        # this is for the gui to change the edge label
-        # self.result.set(caller.result())
+        tmp = caller.result()
+        self.result.set(str(floor(tmp.bandwidth))+" B/s")
+        self.string_result = str(floor(tmp.bandwidth))+" B/s"
+        print(self.string_result)
 
-        # this is for testing from command line
-        self.result = caller.result()
-
-    @staticmethod
-    def create_hash(a, b):
-        """
-            concatenate the strings allows for unique connections given
-            the direction of the communication.
-        """
-        return hash(str(hash(a)).join(str(hash(b))))
+def create_hash(a, b):
+    """
+        concatenate the strings allows for unique connections given
+        the direction of the communication.
+    """
+    return hash(str(hash(a)).join(str(hash(b))))
