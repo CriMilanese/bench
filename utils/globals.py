9b9d385da663fcd51a0ea88a8f85ed394b668329
CriMilanese <cmilanese@pop-os.localdomain>
2020-08-25 05:07:11 +0200

    updates

--- utils/globals.py
+++ utils/globals.py
@@ -1,13 +1,10 @@
-HEIGHT = 800
-WIDTH = 800
+WIN_HEIGHT = 1000
+WIN_WIDTH = 800
+CANVAS_HEIGHT = 400
+CANVAS_WIDTH = 700
 WHITE = '#f4f6ff'
 LIGHT_BLUE = '#127681'
 BLUE = '#10375c'
 GOLD = '#f3c623'
 ERROR_COLOR = '#ffb3b3'
-root = ''
-input_canvas = ''
-frame = ''
-feedback = ''
-network = ''
-results = ''
+root = frame = feedback = network = results = None
