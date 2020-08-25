439d75acde4fc825efefb18594ac54659d7fb71b
CriMilanese <cmilanese@pop-os.localdomain>
2020-08-25 05:06:32 +0200

    more pydoc less print

--- utils/class_network.py
+++ utils/class_network.py
@@ -1,33 +1,46 @@
-from tkinter import Canvas, X, BOTTOM
+from tkinter import Canvas
 from class_node import Node
-from class_edge import Edge
+from class_edge import Edge, create_hash
 from globals import *
 
 class Network():
     # .........
-    def __init__(self, frame):
+    def __init__(self, container):
         self.hosts = []
         self.edges = {}
         self.graph = {}
         self.servers_history = []
-        self.GUI(frame)
+        self.GUI(container)
 
-    def GUI(self, frame):
-        self.canvas = Canvas(frame, height = 300, width=700, bg = LIGHT_BLUE)
-        self.canvas.pack(fill=X, side=BOTTOM)
+    def GUI(self, container):
+        """
+            initiates drawable Canvas where all the geometry is displayed
+        """
+        self.canvas = Canvas(container, height = CANVAS_HEIGHT, width=CANVAS_WIDTH, bg = LIGHT_BLUE)
+        self.canvas.place(relx=0, rely=0.45, relwidth=1, relheight=0.5)
 
     def pop_edge(self, anchor, server, client):
-        link = Edge.create_hash(client, server)
+        """
+            deletes the sketch of the edge from the canvas and its data
+            representation from the virtual structure, it requires the server
+            and client to retrieve the correct line, as each device could have
+            more than one connection
+        """
+        link = create_hash(client, server)
         if link not in self.edges.keys():
             print("not in list")
             return
         self.canvas.delete(self.edges[link].body)
-        self.canvas.delete(self.edges[link].label)
+        self.edges[link].label.destroy()
         del self.edges[link]
 
     def pop_host(self):
+        """
+            deletes the sketch of the node from the canvas and its data
+            representation from the virtual structure
+        """
         if not self.hosts:
-            return
+            raise ValueError("no hosts are present")
         last_host = self.hosts[len(self.hosts)-1]
         if isinstance(last_host, Node):
             self.canvas.delete(last_host.label)
@@ -36,20 +49,24 @@ class Network():
             self.pop_edge()
 
     def add_edge(self, source, dest, lifetime):
-        # self.edges.append(self.canvas.create_line(source.x, source.y, dest.x, dest.y, arrow=LAST))
-        link = Edge.create_hash(source, dest)
+        """
+            draws a line between the nodes coordinates and append its instance
+            to a global dictionary with a unique identifier given by the hash
+            values of the terminals, relative to the order in which they are
+            provided
+        """
+        link = create_hash(source, dest)
         self.edges[link] = Edge(source, dest, lifetime)
         self.edges[link].show(self.canvas, source, dest)
 
     def add_connection(self, server, client, lf):
         """
-            handles both data storage and data visualization
+            handles both data storage and data visualization,
             checks whether the network dictionary already contains server and/or
             client signature(s), creating new instances accordingly
         """
-        print("------------------add clicked----------------------")
-        new_server = Node(server)
-        new_client = Node(client)
+        new_server = Node(server[0], server[1])
+        new_client = Node(client[0], client[1])
         tmp_values = []
         for x in self.graph.values():
             tmp_values.extend(x)
@@ -83,31 +100,30 @@ class Network():
         # keep track of events in order to delete them
         self.servers_history.append(new_server)
 
-        print("debug printing graph")
-        print(self.graph)
-
     def remove_connection(self):
-        print("------------------remove clicked----------------------")
-        tmp = None
-
-        # is there still something to delete?
+        """
+            it follows the server history to delete the last connection added,
+            this might sound redundant but in Python variable names acts like
+            pointers to the same object in memory, hence there is little overhead
+            to assignment the same object more than once
+        """
         if len(self.servers_history)<=0:
             raise ValueError("No more entries to delete")
 
-        # flatten values
+        # flatten values of graph dict
         tmp_values = []
         for x in self.graph.values():
             tmp_values.extend(x)
 
         tmp = self.graph[self.servers_history[-1]]
+        print(tmp)
         last = tmp[-1]
         if tmp_values.count(last) == 1:
-            print("this client had no other connections")
             self.canvas.delete(last.body)
             self.canvas.delete(last.label)
         if len(tmp)==1:
             lonely = None
-            # delete its symbol only if it does not have any other connections
+            # deletes its symbol only if it does not have any other connection
             for key in self.graph.keys():
                 if key == self.servers_history[-1]:
                     self.canvas.delete(key.body)
@@ -119,7 +135,3 @@ class Network():
         tmp.pop()
         self.pop_edge(self.canvas, self.servers_history.pop(), last)
         del tmp
-        for event in self.servers_history:
-            print("servers history: "+str(event))
-        print("debug printing graph")
-        print(self.graph)
--- utils/class_node.py
+++ utils/class_node.py
@@ -1,22 +1,37 @@
-#!/usr/bin/env python3
-# from node_status import status
+"""
+    Virtual representation of a device, or rather a single network card. As with
+    other classes, this one takes care of both the data structure and its
+    visual representation on the main canvas.
+"""
 from random import randint
 from tkinter import Canvas
 from globals import GOLD
 
 class Node():
 
-    def __init__(self, info):
-        self.user = info[0];
-        self.ip = info[1];
+    def __init__(self, username, ip_address):
+        self.user = username;
+        self.ip = ip_address;
         self.server_handle = ''
 
     def show(self, anchor, ind):
+        """
+            depicts the node and its label
+        """
         if isinstance(anchor, Canvas):
             self.x = randint(10,590);
             self.y = randint(10,290);
             self.body = anchor.create_oval(self.x, self.y, self.x, self.y, outline="white", width=10);
             self.label = anchor.create_text(self.x+10, self.y, text=str(ind), fill=GOLD)
+
+    def get_result(self, caller):
+        """
+            callback function only for the node which are given the role of
+            server.
+            TODO: remove it, the server returns nothing as it does not hold
+            any valuable information
+        """
+        self.result = caller.result()
 
     def __eq__(self, other):
         if self.ip==other.ip and self.user == other.user:
@@ -25,7 +40,7 @@ class Node():
             return False
 
     def __ne__(self, other):
-        if self.ip==other.ip and self.user == other.user:
+        if self.ip == other.ip and self.user == other.user:
             return False
         else:
             return True
@@ -34,7 +49,7 @@ class Node():
         return hash(str(self.user+self.ip))
 
     def __str__(self):
-        return str(self.user + " - " + self.ip)
+        return self.user + " - " + self.ip
 
     def __repr__(self):
         return self.user + " - " + self.ip
